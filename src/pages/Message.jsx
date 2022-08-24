104a76a7d31fd57ee5925fc73d62dfddac500b1e
Anurag Prajapati <anuragyuvasoft386@gmail.com>
2022-08-24 18:58:01 +0530

    add fils

--- src/pages/Message.jsx
+++ src/pages/Message.jsx
@@ -1,29 +1,64 @@
-import React, { useContext, useEffect } from 'react'
-import NoteContext from '../Context/NoteContext'
+// import React, { useContext, useEffect } from 'react'
+// import NoteContext from '../Context/NoteContext'
 // import './Message.css'
-const Messages = () => {
-  const title = useContext(NoteContext)
-  useEffect(() => {
-    title.setTitle('Messages Here!!')
-  }, [])
+// const Messages = () => {
+//   const title = useContext(NoteContext)
+//   useEffect(() => {
+//     title.setTitle('Messages Here!!')
+//   }, [])
 
-  // let voice = getVoices()
+//   // let voice = getVoices()
+
+//   return (
+//     <>
+//       <div id="wrapper">
+//         <h1>Simple Text To Speech</h1>
+//         <p id="warning">Sorry, your browser does not support the Web Speech API.</p>
+//         <textarea id="txtFld">I love the sound of my computer-generated voice.</textarea>
+//         <label for="txtFld">Type text above. Then click the Speak button.</label>
+//         <div>
+//           <button type="button" id="speakBtn">Speak</button>
+//           <br />
+//             <p>Note: For best results on a Mac, use the latest version of Chrome, Safari, or FireFox. On Windows, use Chrome.</p>
+//         </div>
+//       </div>
+//     </>
+//   )
+// }
+
+// export default Messages
+
+import {useState} from 'react'
+
+import { Icon } from 'react-icons-kit'
+import {eye} from 'react-icons-kit/feather/eye'
+import {eyeOff} from 'react-icons-kit/feather/eyeOff'
+import './style.css'
+
+function Messages() {
+
+  const [type, setType]=useState('password');
+  const [icon, setIcon]=useState(eyeOff);
+
+  const handleToggle=()=>{    
+    if(type==='password'){
+      setIcon(eye);      
+      setType('text');
+    }
+    else{
+      setIcon(eyeOff);     
+      setType('password');
+    }
+  }
 
   return (
-    <>
-      <div id="wrapper">
-        <h1>Simple Text To Speech</h1>
-        <p id="warning">Sorry, your browser does not support the Web Speech API.</p>
-        <textarea id="txtFld">I love the sound of my computer-generated voice.</textarea>
-        <label for="txtFld">Type text above. Then click the Speak button.</label>
-        <div>
-          <button type="button" id="speakBtn">Speak</button>
-          <br />
-            <p>Note: For best results on a Mac, use the latest version of Chrome, Safari, or FireFox. On Windows, use Chrome.</p>
+    <div className='wrapper'>
+        <div className='input-field'>
+          <input type={type}/>
+          <span onClick={handleToggle}><Icon icon={icon} size={25}/></span>
         </div>
-      </div>
-    </>
-  )
+    </div>
+  );
 }
 
-export default Messages
+export default Messages;
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,4 +1,4 @@
-import React, { useContext  , useEffect } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { useForm } from 'react-hook-form';
 import img from '../Images/img.webp'
 import './style.css'
@@ -6,15 +6,19 @@ import { useDispatch } from 'react-redux';
 import { registerData } from '../redux-Toolkit/reducer/createSlice';
 import { NavLink } from 'react-router-dom';
 import NoteContext from '../Context/NoteContext';
-export default function Form() {
-   
-   const title = useContext(NoteContext)
+import Icon from 'react-icons-kit';
+import { eye } from 'react-icons-kit/feather/eye'
+import { eyeOff } from 'react-icons-kit/feather/eyeOff'
 
-   useEffect(() => {
-     
+export default function Form() {
+
+  const title = useContext(NoteContext)
+
+  useEffect(() => {
+
     title.setTitle("Register Component...")
 
-}, [])
+  }, [])
 
   const dispatch = useDispatch();
 
@@ -25,6 +29,33 @@ export default function Form() {
       data
     ))
   };
+  const [state, setState] = useState({
+    number: null
+  });
+  const onChange = (e) => {
+    var val = e.target.value;
+    setState({
+      number: val.replace(/\W/gi, '').replace(/(.{4})/g, '$1   ')
+    });
+  }
+
+  const [type, setType] = useState('password');
+  const [icon, setIcon] = useState(eyeOff);
+
+  const handleToggle = (e) => {
+    if (type === 'password') {
+      setIcon(eye);
+      setType('text');
+    }
+    else {
+      setIcon(eyeOff);
+      setType('password');
+    }
+    var val = e.target.value;
+    setState({
+      number: val.replace(/\W/gi, '').replace(/(.{4})/g, '$1   ')
+    });
+  }
 
   return (
     <section className='container ' >
@@ -33,22 +64,25 @@ export default function Form() {
           <h2>Register Here!!</h2>
           <hr></hr>
           <form id='form' className='flex flex-col' onSubmit={handleSubmit(onSubmit)}>
-            <input type="text" {...register("name", { required: true  })} placeholder='FullName' />
+            <input type="text" {...register("name", { required: true })} placeholder='FullName' />
             {errors.name?.type === "required" && "Please enter name..."}
             <input type="email" {...register("email", { required: true, })} placeholder='test@gmail.com' />
-            {errors.email?.type === "required" && "Please enter email..." }
+            {errors.email?.type === "required" && "Please enter email..."}
             <input type="password" {...register("password", { required: true, maxLength: 8 })} placeholder='password' />
             {errors.password?.type === "required" && "Please enter password..."}
             {errors.password?.type === "maxLength" && "Password maxLength is 8 "}
-            <input type="date" {...register("date", { required: true})} />
+            <input type="date" {...register("date", { required: true })} />
             {errors.date?.type === "required" && "Please enter Date..."}
-            <input type="number" {...register("number", { required: true , minLength : 10 , maxLength :  10 })} placeholder='mobile number' />
+            <input type="number" {...register("number", { required: true, minLength: 10, maxLength: 10 })} placeholder='mobile number' />
             {errors.number?.type === "required" && "Mobile Number is required"}
             {errors.number?.type === "minLength" && "min Length Exceed"}
             {errors.number?.type === "maxLength" && "Max Length Exceed"}
-
+            <div className='input-field'>
+              <input onChange={onChange} value={state.number} type={type} placeholder="Enter-Adhar-Number" />
+              <span onClick={handleToggle} ><Icon icon={icon} size={18} /></span>
+            </div>
             <button className='btn'>Register</button>
-          <p className='mt-3'>Alredy Have an Accout?<span><NavLink to="/login" >Login</NavLink></span> </p>
+            <p className='mt-3'>Alredy Have an Accout?<span><NavLink to="/login" >Login</NavLink></span> </p>
           </form>
         </div>
         <div className="col-2">
@@ -56,5 +90,5 @@ export default function Form() {
         </div>
       </div>
     </section>
-  ) 
+  )
 }                 
